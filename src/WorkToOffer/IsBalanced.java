e6cec4cbb8e263611fa0fc4802e7cfc35a63f1d0
uestczhuhao <uestczhuhao@163.com>
2017-06-17 15:44:48 +0800

    是否是二叉树

---
+++ src/WorkToOffer/IsBalanced.java
@@ -0,0 +1,26 @@
+package WorkToOffer;
+
+/**
+ * Created by zhuhao on 17-6-17.
+ */
+public class IsBalanced {
+    //后续遍历时，遍历到一个节点，其左右子树已经遍历  依次自底向上判断，每个节点只需要遍历一次
+    private boolean isBalanced=true;
+    public boolean IsBalanced_Solution(TreeNode root) {
+
+        getDepth(root);
+        return isBalanced;
+    }
+    public int getDepth(TreeNode root){
+        if(root==null)
+            return 0;
+        int left=getDepth(root.left);
+        int right=getDepth(root.right);
+
+        if(Math.abs(left-right)>1){
+            isBalanced=false;
+        }
+        return right>left ?right+1:left+1;
+
+    }
+}
