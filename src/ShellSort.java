b986933a16a8bc0a8cb45bf5b39ceacdadc9020b
uestczhuhao <uestczhuhao@163.com>
2017-08-04 11:26:35 +0800

    希尔排序

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -9,24 +9,53 @@ import java.util.Arrays;
 public class ShellSort {
     public static void main(String[] args) {
         int[] a = {10,3,3,54,45,6,17,8,9};
-        ShellSort ( a );
+        shellSort ( a );
         System.out.println ( Arrays.toString (a));
     }
 
-    public static void ShellSort(int[] a){
-        int increment = a.length;
-        int i,j,temp;
-        while (increment >=1) {
+//    public static void ShellSort(int[] a){
+//        int increment = a.length;
+//        int i,j,temp;
+//        while (increment >=1) {
+//            increment = increment/3+1;
+//            for(i=increment;i<a.length;i++){
+//                if(a[i]<a[i-increment]){
+//                    temp = a[i];
+//                    for (j=i-increment;j>=0 && temp<a[j];j-=increment)
+//                        a[j+increment] = a[j];
+//                    a[j+increment] = temp;
+//                }
+//            }
+//            if(increment ==1)
+//                break;
+//        }
+//    }
+
+    private static void shellSort(int[] arr){
+        if (arr == null || arr.length == 0){
+            throw new Error ( "Invalid Input!!!" );
+        }
+
+        int increment = arr.length;
+        while (increment >=1){
             increment = increment/3+1;
-            for(i=increment;i<a.length;i++){
-                if(a[i]<a[i-increment]){
-                    temp = a[i];
-                    for (j=i-increment;j>=0 && temp<a[j];j-=increment)
-                        a[j+increment] = a[j];
-                    a[j+increment] = temp;
+
+            for (int i=increment;i<arr.length;i+=increment){
+                int temp = arr[i];
+                ///判断，只有有逆序对时才会有交换的动作（以increment为跳跃间隔）
+                if (arr[i]<arr[i-increment]){
+                    int j;
+                    ///并非只是交换间隔的两个关键字，例如9...6...3
+                    ///此时应该把3放入9的位置，9放入6的位置，变成3...9...6
+                    ///利用插入排序的思想
+                    for (j=i-increment;j>=0 && temp < arr[j];j-=increment){
+                        arr[j+increment] = arr[j];
+                    }
+                    arr[j+increment] = temp;
                 }
+
             }
-            if(increment ==1)
+            if (increment ==1)
                 break;
         }
     }
