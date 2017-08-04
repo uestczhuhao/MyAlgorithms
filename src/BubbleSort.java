94a4356cf79f4c7ba4a129652afd06fd4a47d2d6
uestczhuhao <uestczhuhao@163.com>
2017-08-04 10:31:01 +0800

    冒泡

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] a = {10,3,3,54,45,6,17,8,9};
-        BubbleSort (a);
+//        BubbleSort (a);
+        bubbleSortCorrect (a);
         System.out.println ( Arrays.toString (a));
     }
 
@@ -19,47 +20,66 @@ public class BubbleSort {
      * 最简单的冒泡排序（初级版）
      * @param list 输入要排序的数组（名）
      */
-    public static void BubbleSort0 (int[] list){
-        int i,j;
-        for (i=0;i<list.length;i++){
-            for (j = i+1;j<list.length;j++){
-                if(list[i] > list[j])
-                    BaseFunctions.swap(list,i,j);
-            }
-        }
-    }
+//    public static void BubbleSort0 (int[] list){
+//        int i,j;
+//        for (i=0;i<list.length;i++){
+//            for (j = i+1;j<list.length;j++){
+//                if(list[i] > list[j])
+//                    BaseFunctions.swap(list,i,j);
+//            }
+//        }
+//    }
 
     /**
      * 正宗的冒泡排序算法
      * @param list  输入要排序的数组（名）
      */
-    public static void BubbleSort(int[] list){
-        int i,j;
-        for (i=0;i<list.length;i++)
-            for (j=list.length-2;j>=i;j--){
-                //j是从后往前循环，
-                // 好处是可以把后面比较小的数据在移动的过程中就提到靠前的位置
-                if(list[j] > list[j+1])
-                    BaseFunctions.swap ( list,j,j+1 );
-            }
-    }
+//    public static void BubbleSort(int[] list){
+//        int i,j;
+//        for (i=0;i<list.length;i++)
+//            for (j=list.length-2;j>=i;j--){
+//                //j是从后往前循环，
+//                // 好处是可以把后面比较小的数据在移动的过程中就提到靠前的位置
+//                if(list[j] > list[j+1])
+//                    BaseFunctions.swap ( list,j,j+1 );
+//            }
+//    }
 
     /**
      * 冒泡排序的改进，设立flag值，减少无用的比较
-     * @param list
      */
-    public static void BubbleSortCorrect(int[] list) {
-        int i,j;
-        boolean flag = true;
-        for(i=0; i<list.length && flag;i++){
-            flag = false;  //初始为false
-            for (j=list.length-2;j>=i;j--){
-                if (list[j] > list[j+1]){
-                    BaseFunctions.swap ( list,j,j+1 );
+//    public static void BubbleSortCorrect(int[] list) {
+//        int i,j;
+//        boolean flag = true;
+//        for(i=0; i<list.length && flag;i++){
+//            flag = false;  //初始为false
+//            for (j=list.length-2;j>=i;j--){
+//                if (list[j] > list[j+1]){
+//                    BaseFunctions.swap ( list,j,j+1 );
+//                    flag = true;
+//                }
+//            }
+//
+//        }
+//    }
+
+    private static void bubbleSortCorrect(int[] arr){
+        if (arr == null || arr.length == 0)
+            throw new Error ( "Invalid Input!!!" );
+
+        for (int i=0;i<arr.length;i++){
+            boolean flag =false;
+            for (int j=arr.length-2;j>=i;j--){
+                if (arr[j+1] < arr[j]){
                     flag = true;
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
                 }
             }
 
+            if (!flag)
+                break;
         }
     }
 
