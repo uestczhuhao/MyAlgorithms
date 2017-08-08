b8fb12a597ff93acb3d24559d0ff350a577bd7ac
uestczhuhao <uestczhuhao@163.com>
2017-08-08 20:29:10 +0800

    巧克力问题AC

--- src/PinDuoDuo/MaxPerform.java
+++ src/PinDuoDuo/MaxPerform.java
@@ -1,10 +1,32 @@
-package HaoWeilai;
+package PinDuoDuo;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by zhuhao on 17-8-1.
+ * 六一儿童节，老师带了很多好吃的巧克力到幼儿园。每块巧克力j的重量为w[j]，对于每个小朋友i，当他分到的巧克力大小达到h[i] (即w[j]>=h[i])，他才会上去表演节目。老师的目标是将巧克力分发给孩子们，使得最多的小孩上台表演。可以保证每个w[i]> 0且不能将多块巧克力分给一个孩子或将一块分给多个孩子。
+ 输入描述:
+
+ 第一行：n，表示h数组元素个数
+ 第二行：n个h数组元素
+ 第三行：m，表示w数组元素个数
+ 第四行：m个w数组元素
+
+
+ 输出描述:
+
+ 上台表演学生人数
+ 示例1
+ 输入
+
+ 3
+ 2 2 3
+ 2
+ 3 1
+ 输出
+
+ 1
  */
 public class MaxPerform {
     public static void main(String[] args) {
