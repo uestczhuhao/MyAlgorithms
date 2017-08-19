283401cb4a02d4d9b9c702bfee43f05ca8a1c892
uestczhuhao <uestczhuhao@163.com>
2017-08-19 22:09:11 +0800

    三角形问题

--- src/BaiDu/MaxTriangle.java
+++ src/BaiDu/MaxTriangle.java
@@ -1,5 +1,9 @@
 package BaiDu;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by zhuhao on 17-8-19.
  * 三维空间中有N个点，每个点可能是三种颜色的其中之一，三种颜色分别是红绿蓝，分别用'R', 'G', 'B'表示。
@@ -29,4 +33,53 @@ package BaiDu;
  6.00000
  */
 public class MaxTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner ( System.in );
+        while (sc.hasNext ()){
+            int n = sc.nextInt ();
+            String[] input = new String[n];
+            char[] c = new char[n];
+            int[] x = new int[n];
+            int[] y = new int[n];
+            int[] z = new int[n];
+            sc.nextLine ();
+            for (int i=0;i<n;i++){
+                String tmp =sc.nextLine ();
+                input[i] = tmp;
+                String[] strs = tmp.split ( " " );
+                c[i] = strs[0].charAt ( 0 );
+                x[i] = Integer.parseInt ( strs[1] );
+                y[i] = Integer.parseInt ( strs[2] );
+                z[i] = Integer.parseInt ( strs[3] );
+            }
+            double maxArea = 0;
+            for (int i=0;i<n;i++){
+                for (int j=0;j<n;j++){
+                    for (int k=0;k<n;k++){
+                        if ((c[i] == c[j] && c[i] == c[k]) ||
+                                (c[i]!=c[j] && c[i]!=c[k] && c[j]!=c[k])){
+                            double area = countArea(x[i],y[i],z[i],x[j],y[j],z[j],x[k],y[k],z[k]);
+                            if (area > maxArea)
+                                maxArea = area;
+                        }
+                    }
+                }
+            }
+//            DecimalFormat df = new DecimalFormat ( "#.00000" );
+//            System.out.println (df.format ( maxArea ));
+            System.out.format ( "%.5f",maxArea );
+        }
+    }
+
+    public static double countArea(int x2,int y2,int z2,int x1,int y1,int z1,int x3,int y3,int z3){
+        double a = distance ( x1,y1,z1,x2,y2,z2 );
+        double b = distance ( x1,y1,z1,x3,y3,z3 );
+        double c = distance ( x3,y3,z3,x2,y2,z2 );
+        double s = (a+b+c)/2;
+        return Math.sqrt ( s*(s-a)*(s-b)*(s-c) );
+    }
+
+    public static double distance(int x2,int y2,int z2,int x1,int y1,int z1){
+        return Math.sqrt ( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2) );
+    }
 }
