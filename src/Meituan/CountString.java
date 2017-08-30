373b0be8b49224906e94097152b2dc669e994da7
uestczhuhao <uestczhuhao@163.com>
2017-08-30 16:33:44 +0800

    进制法解决字符串统计个数问题

--- src/Meituan/CountString.java
+++ src/Meituan/CountString.java
@@ -29,6 +29,12 @@ public class CountString {
             String s2 = sc.next ();
             int len1 = sc.nextInt ();
             int len2 = sc.nextInt ();
+            System.out.println (count2 ( s1,s2,len1,len2 ));
+            System.out.println ();
+            System.out.println ();
+            System.out.println ();
+            System.out.println ();
+            System.out.println ();
             long result = 0;
             char[] chs1 = s1.toCharArray ();
             char[] chs2 = s2.toCharArray ();
@@ -51,6 +57,35 @@ public class CountString {
         }
     }
 
+    //利用进制法，例如求15 46之间的数，则利用（4-1）×10+（6-1）×1
+    //小写字母可以看作是26进制的数
+    public static long count2(String s1,String s2,int len1,int len2){
+        StringBuffer st1 = new StringBuffer ( s1 );
+        StringBuffer st2 = new StringBuffer ( s2 );
+
+        char ch = 'a'-1;
+        //把字符串扩展到len2位，不足的补'a'-1，相当于十进制补0操作
+        for (int i=s1.length ();i<len2;i++){
+            st1.append ( ch );
+        }
+        for (int i = s2.length ();i<len2;i++){
+            st2.append ( ch );
+        }
+        char[] chs1 = st1.toString ().toCharArray ();
+        char[] chs2 = st2.toString ().toCharArray ();
+        int[] dif = new int[len2];
+        for (int i=0;i<len2;i++)
+            dif[i] = chs2[i] - chs1[i];
+        int result=0;
+        for (int i=len1;i<=len2;i++){
+            for (int j=0;j<i;j++){
+                result += dif[j]*(int)Math.pow ( 26,i-j-1 );
+                if (result>=1000007)
+                    result %= 1000007;
+            }
+        }
+        return result-1;
+    }
 //    public static void main(String[] args) {
 //        System.out.println (Math.pow ( 26,45 ));
 //        System.out.println ((long) Math.pow ( 26,45 ));
