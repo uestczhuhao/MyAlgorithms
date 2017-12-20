00691bccfbb70ac862685aec031526d63c88a19a
uestczhuhao <uestczhuhao@163.com>
2017-12-20 15:33:33 +0800

    第五题：最长回文学历

--- src/LeetCode/_5LongestPalinSubstr.java
+++ src/LeetCode/_5LongestPalinSubstr.java
@@ -14,6 +14,8 @@ public class _5LongestPalinSubstr {
         int x=0,y=0;
         int max = 0;
         //i要从后往前扫描，j从前往后扫描，dp[i][j] = true代表从i到j为回文
+        //因为i,j处的数据要用到 i+1,j-1处的数据
+        //因此二维矩阵要从下往上，从左往右建立
         for (int i=s.length()-1;i>=0;i--){
             for (int j=i;j<s.length();j++){
                 if (s.charAt(j) == s.charAt(i) && (j-i<=2 || dp[i+1][j-1])) {
